858a6087bacf31f2dca760449a1ae93001eff006
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2023-01-16 13:16:21 +0800

    TRAIN-941: create Add Todo [Template]

---
+++ src/components/templates/AddTodo/AddTodo.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import { Header } from '../../molecules/Header';
+import { TextInput } from '../../atoms/TextInput';
+import { ReactComponent as SearchIcon } from '../TodoHome/icons/search.svg';
+import { Toast } from '../../atoms/Toast';
+
+import {
+    Wrapper,
+    SearchWrapper,
+} from './styles';
+
+export type AddTodoProps = {
+    onAddTodo?: (e: string) => void;
+    onBack?: (e: any) => void;
+};
+
+const AddTodo = ({ onAddTodo = () => {}, onBack }: AddTodoProps) => {
+    const [todo, setTodo] = useState("");
+    const [showToast, setShowToast] = useState(false);
+
+    const onPressEnterHandler = () => {
+        onAddTodo(todo);
+        setTodo("");
+        setShowToast(true);
+    };
+
+    return (
+        <Wrapper>
+            <Header title="Add To do" type="navigation" onBack={onBack} />
+            <SearchWrapper>
+                <TextInput 
+                    value={todo}
+                    prefix={<SearchIcon />} 
+                    onChange={e => setTodo(e.currentTarget.value)} 
+                    onPressEnter={onPressEnterHandler} 
+                    allowClear 
+                />
+            </SearchWrapper>
+            <Toast 
+                message="To do saved" 
+                duration={2} 
+                show={showToast} 
+                onClose={() => setShowToast(false)} 
+            />
+        </Wrapper>
+    );
+};
+
+export default AddTodo; 
