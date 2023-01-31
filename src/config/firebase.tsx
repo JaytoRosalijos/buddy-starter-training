e8892c4a688072ccbe4f1a18f81a9a3afd66b6e6
yehey1999 <47969048+yehey1999@users.noreply.github.com>
2023-01-31 13:05:14 +0800

    TRAIN-984-985-998: store api keys/details for firebase in env file

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { browserLocalPersistence, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtgsGNM_mRAiGA_hvFEiliiHw6dgTMMHo",
-  authDomain: "buddy-starter-training-api.firebaseapp.com",
-  projectId: "buddy-starter-training-api",
-  storageBucket: "buddy-starter-training-api.appspot.com",
-  messagingSenderId: "1013790877134",
-  appId: "1:1013790877134:web:ddf5b4cc339fd6d93fb79f",
-  measurementId: "G-JMC61VGZY5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MESSUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
